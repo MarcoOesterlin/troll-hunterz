e9ca1f9b776e6ae072db1d037689d92954b9f924
Sven Lindqvist <Sven-Lindqvist@hotmail.com>
2019-01-11 10:55:12 +0100

    Header animation, listitem style

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,11 +5,23 @@ import EntryBanner from "../EntryBanner/EntryBanner";
 
 class Header extends React.Component {
   render() {
+    const scrollToTop = () => {
+      const c = document.documentElement.scrollTop || document.body.scrollTop;
+      if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 8);
+      }
+    };
+
     return (
       <header className={`${this.props.display}`}>
         <label htmlFor="textfield">
           <h1>
-            <img src={TrollLogo} alt="Troll Hunterz Logo" />
+            <img
+              src={TrollLogo}
+              alt="Troll Hunterz Logo"
+              onClick={() => scrollToTop()}
+            />
           </h1>
         </label>
         {this.props.children}
--- server/Controller.js
+++ server/Controller.js
@@ -1,41 +1,46 @@
-import { uri } from './mongodbConfig.js';
-import { MongoClient } from 'mongodb';
-import axios from 'axios';
-import { sentimentURI } from './config';
-import youtubeAPIKey from './youtubeAPIKey';
-import {google} from 'googleapis';
+import { uri } from "./mongodbConfig.js";
+import { MongoClient } from "mongodb";
+import axios from "axios";
+import { sentimentURI } from "./config";
+import youtubeAPIKey from "./youtubeAPIKey";
+import { google } from "googleapis";
 export default class Controller {
   constructor() {
-    this.mongoClient = new MongoClient(
-      uri,
-      { useNewUrlParser: true },
-    );
+    this.mongoClient = new MongoClient(uri, { useNewUrlParser: true });
     this.mongoClient.connect();
     this.getAllEntries = this.getAllEntries.bind(this);
     this.insertEntry = this.insertEntry.bind(this);
     this.youtubeEntry = this.youtubeEntry.bind(this);
   }
-  
+
   getAllEntries(_req, res) {
     res.status(200).send({
       entries: {
-        toxic: [{
-          imageUrl: 'https://yt3.ggpht.com/a-/AAuE7mAcW198VrHrQqGTGs5EEXx7-Tv-qtFpWky1og=s88-mo-c-c0xffffffff-rj-k-no',
-          score: -7.182177975501057,
-          username: 'newdramaalert',
-        },{
-          imageUrl: 'https://yt3.ggpht.com/a-/AAuE7mD7iJevCngNeCAIs0-3F6Dn_L4LtFnVOcpp9w=s88-mo-c-c0xffffffff-rj-k-no',
-          score: 43.38968802213832,
-          username: 'h3h3productions',
-        }],
-        polite: [{
-          imageUrl: 'https://yt3.ggpht.com/a-/AAuE7mDZhAzA6_0IvivO8I50ZE8Wdw3XQY4v2V-nDA=s88-mo-c-c0xffffffff-rj-k-no',
-          score: 69.38185887861775,
-          username: 'destinws2',
-        }],
+        toxic: [
+          {
+            imageUrl:
+              "https://yt3.ggpht.com/a-/AAuE7mAcW198VrHrQqGTGs5EEXx7-Tv-qtFpWky1og=s88-mo-c-c0xffffffff-rj-k-no",
+            score: -7.182177975501057,
+            username: "newdramaalert"
+          },
+          {
+            imageUrl:
+              "https://yt3.ggpht.com/a-/AAuE7mD7iJevCngNeCAIs0-3F6Dn_L4LtFnVOcpp9w=s88-mo-c-c0xffffffff-rj-k-no",
+            score: 43.38968802213832,
+            username: "h3h3productions"
+          }
+        ],
+        polite: [
+          {
+            imageUrl:
+              "https://yt3.ggpht.com/a-/AAuE7mDZhAzA6_0IvivO8I50ZE8Wdw3XQY4v2V-nDA=s88-mo-c-c0xffffffff-rj-k-no",
+            score: 69.38185887861775,
+            username: "destinws2"
+          }
+        ]
       }
     });
-    
+
     // const { mongoClient } = this;
     // const collection = mongoClient.db('trollhunterz').collection('entries');
     // collection.find({}).toArray((err, result) => {
@@ -52,70 +57,72 @@ export default class Controller {
     // });
   }
 
-  async insertEntry (req, res) {
+  async insertEntry(req, res) {
     const { mongoClient } = this;
-    const collection = mongoClient.db('trollhunterz').collection('entries');
+    const collection = mongoClient.db("trollhunterz").collection("entries");
     const { value } = req.body;
     if (!value) {
-      res.status(400).send('Empty body');
+      res.status(400).send("Empty body");
       return;
     }
-  
+
     try {
-      const analyzeResponse = await axios.get(`${ sentimentURI }/analyze?value=${ value }`);
+      const analyzeResponse = await axios.get(
+        `${sentimentURI}/analyze?value=${value}`
+      );
       const { comparative, score } = analyzeResponse.data;
       try {
         collection.insertOne({
           _id: {
-            value,
+            value
           },
           comparative,
-          score,
+          score
         });
         res.status(200).send({
           value,
           comparative,
-          score,
+          score
         });
         return;
       } catch (e) {
-        res.status(500).send('Database failure.');
+        res.status(500).send("Database failure.");
         return;
       }
     } catch (err) {
-      res.status(500).send(`Failed to analyze.${ process.env.SENTIMENT_URI }`);
+      res.status(500).send(`Failed to analyze.${process.env.SENTIMENT_URI}`);
       return;
     }
   }
 
   async youtubeEntry(req, res) {
-    const service = google.youtube('v3');
+    const service = google.youtube("v3");
     const { username } = req.body;
 
     const usernameToChannelId = async username => {
       const parameters = {
-        'maxResults': '25',
-        'part': 'snippet',
-        'auth': youtubeAPIKey,
-        'forUsername': username,
+        maxResults: "25",
+        part: "snippet",
+        auth: youtubeAPIKey,
+        forUsername: username
       };
       try {
         const googleResponse = await service.channels.list(parameters);
         const channelId = googleResponse.data.items[0].id;
         return channelId;
-      } catch (e) {
+      } catch (e) {
         return null;
       }
     };
 
     const channelIdToVideoIds = async (channelId, maxResults) => {
       const parameters = {
-        'maxResults': maxResults,
-        'part': 'snippet',
-        'auth': youtubeAPIKey,
-        'type': 'video',
-        'channelId': channelId,
-        'order': 'viewCount',
+        maxResults: maxResults,
+        part: "snippet",
+        auth: youtubeAPIKey,
+        type: "video",
+        channelId: channelId,
+        order: "viewCount"
       };
       try {
         const googleResponse = await service.search.list(parameters);
@@ -126,76 +133,81 @@ export default class Controller {
       } catch (e) {
         return null;
       }
-    }
-    
+    };
+
     const retrieveComments = async (videoId, maxResults) => {
       const parameters = {
-        'maxResults': maxResults,
-        'part': 'snippet',
-        'auth': youtubeAPIKey,
+        maxResults: maxResults,
+        part: "snippet",
+        auth: youtubeAPIKey,
         videoId,
-        'order': 'relevance',
+        order: "relevance"
       };
       try {
         const googleResponse = await service.commentThreads.list(parameters);
-        const { data } = googleResponse;
+        const { data } = googleResponse;
         const { items } = data;
-        const comments = items.map(item => item.snippet.topLevelComment.snippet.textOriginal);
+        const comments = items.map(
+          item => item.snippet.topLevelComment.snippet.textOriginal
+        );
         return comments;
       } catch (e) {
         return [];
       }
-    }
-    
+    };
+
     const summarizeVideoComments = async videoComments => {
-      const commentScores = await Promise.all(videoComments.map(async videoComment => {
-        const sentimentResponse = await axios.get(
-          `${ sentimentURI }/analyze?value=${ encodeURI(videoComment) }`
-        );
-        return sentimentResponse.data.comparative;
-      }));
+      const commentScores = await Promise.all(
+        videoComments.map(async videoComment => {
+          const sentimentResponse = await axios.get(
+            `${sentimentURI}/analyze?value=${encodeURI(videoComment)}`
+          );
+          return sentimentResponse.data.comparative;
+        })
+      );
       const sumScores = commentScores.reduce((a, b) => a + b, 0);
       return sumScores;
     };
 
     const getImageUrl = async username => {
       const parameters = {
-        'maxResults': '25',
-        'part': 'snippet',
-        'auth': youtubeAPIKey,
-        'forUsername': username,
+        maxResults: "25",
+        part: "snippet",
+        auth: youtubeAPIKey,
+        forUsername: username
       };
       try {
         const googleResponse = await service.channels.list(parameters);
-        const imageUrl = googleResponse.data.items[0].snippet.thumbnails.default.url;
+        const imageUrl =
+          googleResponse.data.items[0].snippet.thumbnails.default.url;
         console.log(imageUrl);
         return imageUrl;
       } catch (e) {
-        console.log('Image not found');
+        console.log("Image not found");
         return null;
       }
-    }
-    
+    };
+
     const channelId = await usernameToChannelId(username);
     if (!channelId) {
-      res.status(400).send('Channel not found.');
+      res.status(400).send("Channel not found.");
       return;
     }
     const videoIds = await channelIdToVideoIds(channelId, 10);
     const videosComments = await Promise.all(
-      videoIds.map(videoId => retrieveComments(videoId, 100)),
+      videoIds.map(videoId => retrieveComments(videoId, 100))
     );
     const videoSentimentScores = await Promise.all(
-      videosComments.map(videoComments => summarizeVideoComments(videoComments),
-    ));
+      videosComments.map(videoComments => summarizeVideoComments(videoComments))
+    );
     const channelSentimentSum = videoSentimentScores.reduce((a, b) => a + b, 0);
 
     const imgUrl = await getImageUrl(username);
-    
+
     res.status(200).send({
       score: channelSentimentSum,
       username: username,
-      imgUrl: imgUrl,
+      imgUrl: imgUrl
     });
   }
 }
